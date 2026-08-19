8a1548c85bd49da2e2277a2816bf1782c558874e
Devansh Chandra <155964011+Devansh149@users.noreply.github.com>
2026-08-20 01:53:50 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,5 +1,9 @@
 import express from 'express'
-
+import authRouter from './routes/auth.routes.js'
+import cookieparser from 'cookie-parser'
 const app=express()
+app.use(express.json())
+app.use(cookieparser())
+app.use("/api/auth",authRouter)
 
 export default app
