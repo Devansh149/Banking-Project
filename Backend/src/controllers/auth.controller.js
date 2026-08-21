ec2973b8597d6f3071fd2f185aab182a5d21967f
Devansh Chandra <155964011+Devansh149@users.noreply.github.com>
2026-08-21 14:43:50 +0530

    Add loginController to auth.controller.js

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -30,4 +30,39 @@ async function registerUser(req, res) {
     })
 }
 
-export default { registerUser }
+async function loginController(req,res){
+    const {email,password}=req.body
+    const user=await userModel.findOne({email}).select("+password")
+
+    if(!user){
+        return res.status(401).json({
+            message:"User not found"
+        })
+    }
+    
+    const isValidPassword=await user.comparePassword(password)
+     
+    if(!isValidPassword){
+        return res.status(401).json({
+            message:"Enter valid password"
+        })
+
+    }
+    const token=jsonwebtoken.sign({userId:user._id},process.env.JWT_SECRET,{expiresIn:"3d"})
+
+    res.cookie("token",token)
+
+    return res.status(200).json({
+        message:"User logged in successfully",
+        user:{
+            id:user._id,
+            email:user.email,
+            name:user.name
+
+        }
+    })
+
+    
+}
+
+export default { registerUser , loginController}
